9a35bcc146e4309339b14b241a686b19bd5a236b
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-01 17:39:27 +0300

    Zadanie1

---
+++ HomeWorkTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter first number a = ");     //Vvodim chislo a
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number b = ");    //Vvodim chislo b
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;                                  //VVodim peremennyu max, v nee bydem zapisivat' maksimal'noe znachenie 
+if (a > b)                                    // Proveryaem yslovie
+{
+    max = a;                                  // Esli yslovie vypolnyaetsya prisvaevaem max znachenie a
+    Console.Write("a > b, max = ");
+    Console.WriteLine(max);
+}
+else
+{
+    max = b;
+    Console.Write("a < b, max = ");
+    Console.WriteLine(max);
+}

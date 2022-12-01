2555ba459b7715e06352f5c64d70d167721bbc0e
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-01 19:52:30 +0300

    Zadanie2

---
+++ HomeWorkTask2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Enter 3 number");
+Console.Write("Enter first number a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number b =");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number c =");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > b) 
+{
+  max = a;
+  if (max > c)
+  {
+    Console.WriteLine("a maximum number");
+    Console.WriteLine(max);
+  }
+  else
+  {
+    max = c;
+    Console.WriteLine("c maximum number");
+    Console.WriteLine(max);
+  }
+}
+else 
+{
+    max = b;
+    if (max > c)
+    {
+      Console.WriteLine("b maximum number");
+      Console.WriteLine(max);  
+    }
+    else 
+    {
+      max = c;
+      Console.WriteLine("c maximum number");
+      Console.WriteLine(max);
+    }
+}

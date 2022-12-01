1234763b277e829d73de8120fe8791b445e80e6f
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-01 20:08:59 +0300

    Zadanie3

---
+++ HomeWorkTask3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number a = ");    
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+{
+    Console.WriteLine ("Number is even");
+}
+else 
+{
+    Console.WriteLine ("Number is odd");
+}

0e36cef63c7d24360e05cffcc9ee2c08730e6e88
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-03 01:25:25 +0300

    Zadanie6

---
+++ HomeWorkTask6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter number a = ");  
+int number = Convert.ToInt32(Console.ReadLine());
+int ost = 0;
+if (number / 100 == 0)
+{
+    Console.WriteLine("Resheniy net");
+}
+else
+{
+    while (number / 1000 != 0)
+        number = number / 10;
+    ost = (number % 100) % 10; 
+}
+   Console.WriteLine(ost);
+
+

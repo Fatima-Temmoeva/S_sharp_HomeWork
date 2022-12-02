f73598e9ec471747ccf5a0abff9a60a75a5bb0ba
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-03 02:27:08 +0300

    Zadanie8

---
+++ HomeWorkTask8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter number a = ");  
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 10000 == 0)
+{
+    Console.WriteLine("Resheniy net");
+}
+else
+{
+    if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+    {
+      Console.WriteLine("Chislo - palindrome");  
+    }
+    else
+    {
+      Console.WriteLine("Chislo - NE palindrome");  
+    }
+}
+   

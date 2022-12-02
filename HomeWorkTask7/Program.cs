78ded21fb492404cde793fb4ec743a9f0294c67e
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-03 00:16:05 +0300

    Zadanie7.1

---
+++ HomeWorkTask7/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Enter day of the week");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case 1:
+    Console.WriteLine("Today is Monday. Today is a working day");
+    break;
+
+    case 2:
+    Console.WriteLine("Today is Tuesday. Today is a working day");
+    break;
+
+    case 3:
+    Console.WriteLine("Today is Wensday. Today is a working day");
+    break;
+
+    case 4:
+    Console.WriteLine("Today is Thursday. Today is a working day");
+    break;
+
+    case 5:
+    Console.WriteLine("Today is Friday. Today is a working day");
+    break;
+
+    case 6:
+    Console.WriteLine("Today is Saturday. Today is a day off ");
+    break;
+
+    case 7:
+    Console.WriteLine("Today is Sunday. Today is a day off");
+    break;
+
+    default:
+    Console.WriteLine("Invalid value of day of the week");
+    break;
+}

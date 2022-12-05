21d5ab6af31c9d7d758d59b43a30af93e8c328c0
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-05 23:06:53 +0300

    Zadanie10

---
+++ HomeWorkTask11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Vvedi chislo dlya vozvedeniya v stepen'");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedi stepen'");
+int B = Convert.ToInt32(Console.ReadLine());
+int st = 1;
+for (int i = 1; i <= B; i++ )
+st = st * A;
+Console.WriteLine("Stepen' chisla A = " + st);
+

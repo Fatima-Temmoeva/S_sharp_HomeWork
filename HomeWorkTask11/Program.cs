c6cbbcd03971d39d7a39eac7bcceacb0368adbab
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-06 22:13:05 +0300

    Zadanie11_usefunction

--- HomeWorkTask11/Program.cs
+++ HomeWorkTask11/Program.cs
@@ -2,8 +2,15 @@
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Vvedi stepen'");
 int B = Convert.ToInt32(Console.ReadLine());
-int st = 1;
-for (int i = 1; i <= B; i++ )
-st = st * A;
+
+int Stepen(int chislo1, int chislo2)
+{
+    int stepen = 1;
+    for (int i = 1; i <= chislo2; i++)
+        stepen = stepen * chislo1;
+    return stepen;
+}
+
+int st = Stepen(A, B);
 Console.Write("Stepen' chisla A = " + st);
 Console.WriteLine();

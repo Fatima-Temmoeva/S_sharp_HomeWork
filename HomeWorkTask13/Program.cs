b94fcd10d134b995a34b313ca416815c56fce0f0
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-07 00:23:14 +0300

    Zadanie13

---
+++ HomeWorkTask13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter massiv size");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] massiv = new int[n];
+int i = 0;
+Console.WriteLine("Enter massiv elements");
+while (i < massiv.Length)
+{
+    massiv[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+Console.Write("[");
+for (i = 0; i < massiv.Length; i++)
+{
+    if (i == massiv.Length - 1)
+    {
+        Console.Write(massiv[i]);
+    }
+    else
+    {
+        Console.Write(massiv[i] + " ");
+    }
+}
+Console.Write("]");

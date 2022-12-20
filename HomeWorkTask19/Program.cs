1f0b27850c210b9d13c3009990e348060b78ab4f
Fatimq Temmoeva <fatima94temmoeva@gmail.com>
2022-12-20 12:41:43 +0300

    Zadanie19

---
+++ HomeWorkTask19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Razmernost' massiva");
+Console.WriteLine("Vvedi kolichestvo strok n=");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedi kolichestvo stolbtsov m=");
+int m = Convert.ToInt32(Console.ReadLine());
+double [,] mass = new double [n, m];
+Random random = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j =0; j < m; j++)
+    {
+        mass[i, j] = random.NextDouble()*10;
+    }
+}
+    
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(String.Format("{0,6:F2}", mass[i, j]));
+    }
+    Console.WriteLine();
+}
+   
